34dc9ca0bc33a184c0be6e27be6b7022fd168cf2
JonyPeixoto <jonathanppeixoto@gmail.com>
2020-10-12 19:31:07 -0300

    update 12out

---
+++ basic.py
@@ -0,0 +1,14 @@
+#name = "Jonathan"
+#age = "24"
+#numero = "121" // It is a text
+#numero = 121 // It is a number
+
+number1 = 20
+number2 = 20
+number3 = 15
+
+total = number1 + number2 + number3 + 5
+
+    print(total + 40)
+#print(total + 40)
+
